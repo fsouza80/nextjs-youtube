c01c0523412c24d8bb530d4733367c5037c722f8
fsouza80 <felipe.asouza@hotmail.com.br>
2021-02-24 18:25:15 -0300

    Material-UI configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import { Button } from '@material-ui/core'
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
-      Clone Youtube  
+      Clone Youtube
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   )
 }
